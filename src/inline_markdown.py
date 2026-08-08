806ced037b2ea2262e95855aaad451cb315a1df6
hseyinblgc <h.bilgic1818@gmail.com>
2026-08-08 17:29:27 +0300

    feat(inline-markdown): add functions to extract images and links
    - Implement `extract_markdown_images` to find `![alt](url)` patterns
    - Implement `extract_markdown_links` to find `[text](url)` patterns
    - Add unit tests for image and link extraction, including ignoring images for link extraction

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,7 +1,11 @@
+import re
+
 from textnode import TextNode, TextType
 
 
-def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
+def split_nodes_delimiter(
+    old_nodes: list[TextNode], delimiter: str, text_type: TextType
+) -> list[TextNode]:
     new_nodes: list[TextNode] = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
@@ -10,13 +14,15 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
 
         sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
-            raise ValueError(f"Invalid markdown syntax: formatted section with '{delimiter}' not closed")
+            raise ValueError(
+                f"Invalid markdown syntax: formatted section with '{delimiter}' not closed"
+            )
 
         split_nodes = []
         for i in range(len(sections)):
             if sections[i] == "":
                 continue
-            
+
             if i % 2 == 0:
                 split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
@@ -25,3 +31,11 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
         new_nodes.extend(split_nodes)
 
     return new_nodes
+
+
+def extract_markdown_images(text: str) -> list[tuple]:
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text: str) -> list[tuple]:
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from textnode import TextNode, TextType
 
 
@@ -75,6 +79,33 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is `unclosed code block", TextType.TEXT)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+
+class TestMarkdownParser(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links_ignores_images(self):
+        text = "Here is an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(
+            [("link", "https://boot.dev")],
+            matches,
+        )
 
 
 if __name__ == "__main__":
