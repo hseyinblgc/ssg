709c2adcc9b8a75db9c2e8becee63b7096c796d3
hseyinblgc <h.bilgic1818@gmail.com>
2026-08-11 22:02:43 +0300

    feat(generator): implement static asset copying
    - Add `copy_folder` utility to recursively copy files.
    - Configure `main` to copy `static/` content to `public/`.
    - Remove outdated `public/index.html` and `public/styles.css`.
    - Introduce new `static/` directory with styling and images.
    - Update `.gitignore` to exclude `public/` directory.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
-from textnode import TextNode
+import os
+import shutil
 
 
-def main():
-    print(TextNode("This is some anchor text.", "link", "https://www.google.com"))
+def copy_folder(src: str, dst: str) -> None:
+    work_dir: str = os.getcwd()
+    src_dir: str = os.path.normpath(os.path.join(work_dir, src))
+    dest_dir: str = os.path.normpath(os.path.join(work_dir, dst))
+
+    try:
+        items: list[str] = os.listdir(src_dir)
+        if len(items) == 0:
+            return
+    except FileNotFoundError:
+        return
+
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+    os.mkdir(dest_dir)
+
+    for item in items:
+        src_file: str = os.path.join(src_dir, item)
+        dest_file: str = os.path.join(dest_dir, item)
+        if os.path.isfile(path=src_file):
+            print(f"{shutil.copy(src_file, dest_file)}")
+        elif os.path.isdir(src_file):
+            copy_folder(src_file, dest_file)
+
+
+def main() -> None:
+    copy_folder(src="static", dst="public")
 
 
 if __name__ == "__main__":
